e782281a70058eecc5b0614a9b0e0747b32f3e6d
LeeMinyoung-1120 <nyam71628@gmail.com>
2026-01-13 21:11:33 +0900

    express test

--- shopping-backend/index.js
+++ shopping-backend/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
+
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello Express!');
@@ -8,4 +17,10 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
+});
+
+// Post Method 만들기 (return 값)
+app.post('/api/pay', (req, res) => {
+  console.log(req.body);
+  res.json({ success: true });
 });
